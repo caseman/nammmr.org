96825e0f47e9b35e7f5124d0a3c5916ef455a474
Casey Duncan <casey.duncan@coinbase.com>
2019-02-18 14:22:44 -0700

    if-changed hbs helper

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,26 @@
+// if-changed helper only renders contents if value differs from last invocation
+function ifChanged(value, options) {
+    if (value !== options.data.__ifChanged_last) {
+        options.data.__ifChanged_last = value;
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+}
+
 module.exports = function(eleventyConfig) {
     [
         "img",
         "thumb",
+        "photos",
         "css",
         "js",
         "favicon.jpg",
         "site.webmanifest",
         "robots.txt",
     ].forEach(path => eleventyConfig.addPassthroughCopy(path));
+
+    eleventyConfig.addHandlebarsHelper('if-changed', ifChanged);
 
     return {
         passthroughFileCopy: true
