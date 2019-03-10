f8dcaf214ce84a5db3e7ccd87ff24eb429fb2fd1
Casey Duncan <casey.duncan@coinbase.com>
2019-03-09 17:17:39 -0700

    floating jump links on for-sale

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 if (typeof documentReady === 'function') {
     documentReady();
 }
+
+$(document).ready(function () {
+    var pinned = $('#pin-to-top');
+    if (pinned) {
+        var top = pinned.offset().top - parseFloat(pinned.css('marginTop'));
+
+        $(window).scroll(function (event) {
+            var ypos = $(this).scrollTop();
+            console.log(ypos, top, pinned.offset());
+            if (ypos >= top) {
+                pinned.addClass('pinned');
+            } else {
+                pinned.removeClass('pinned');
+            }
+        });
+    }
+});
+
